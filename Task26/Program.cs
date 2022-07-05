e95ea10731618259e551652586cbf85798fa4c27
Vadim <uvolkov@374@gmail.com>
2022-07-05 14:41:40 +0300

    Add Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = InputInt("Введите число: ");
+
+int b = 0;
+int sum = 0;
+
+int count = 0;
+
+while(a > 0)
+{
+    b = a % 10;
+    sum = sum + b;
+    a = a / 10;
+    count++;
+}
+
+ Console.WriteLine(sum);
+
+
+int InputInt(string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
