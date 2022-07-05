3952ebdf655c713d388e8c439d934a161e7c8121
Vadim <uvolkov@374@gmail.com>
2022-07-05 14:25:15 +0300

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = InputInt("Enter first number: ");
+int b = InputInt("Enter second number: ");
+
+int step = 1;
+
+
+
+for(int i = 0; i < b; i++)
+{
+    step *= a;
+}
+Console.WriteLine(step);
+
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
